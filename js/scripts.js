c8e848e53c4401a61a1ddc1644882945c85707b2
Mohamed Mohamed <demahom93@yahoo.co.uk>
2020-02-12 19:54:00 +0300

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+function akanName (birthday, sex, ){
+  return
+}
+getDay
+var yearOfBirth = parseInt(prompt("Enter your year of Birth"));
+var mmonthOfBirth = parseInt(prompt("Enter your month of Birth"));
+var dayOfBirth = parseInt(prompt ("Enter your day of Birth"));
+
+
+if (birthday === 00 && sex === male)
+console.log('Kwasi');
